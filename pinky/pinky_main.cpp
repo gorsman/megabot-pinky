0cddd24de4e1c616a2a7b1c210398616a3a4326d
Igor Kulkin <igor.kulkin@gmail.com>
2013-05-27 06:54:26 +0200

    changed implementation of motor controller

--- pinky/pinky_main.cpp
+++ pinky/pinky_main.cpp
@@ -6,7 +6,7 @@
 
 #include <ros.h>
 #include <std_msgs/Empty.h>
-#include <std_msgs/Int32.h>
+#include <std_msgs/Int32MultiArray.h>
 
 #include "example/example.h"
 #include "motor.h"
@@ -21,28 +21,39 @@ void messageCb(const std_msgs::Empty& toggle_msg){
 }
 ros::Subscriber<std_msgs::Empty> sub("blink_led", &messageCb );
 
-std_msgs::Int32 log_msg;
+#define LOG_DATA_LEN 5
+std_msgs::Int32MultiArray log_msg;
+int32_t logData[LOG_DATA_LEN];
 ros::Publisher logger("logger", &log_msg);
 
-void logMsg(int32_t x) {
-  log_msg.data = x;
+void log(int32_t v0, int32_t v1, int32_t v2, int32_t v3, int32_t v4) {
+  logData[0] = v0;
+  logData[1] = v1;
+  logData[2] = v2;
+  logData[3] = v3;
+  logData[4] = v4;
   logger.publish( &log_msg );
 }
 
 Motor& motor = Motor::LEFT;
 MotorController motorController(motor);
 
+long startTime = 0;
 long lastTrigger = 0;
-long triggerPeriod = 200;
+long triggerPeriod = 20;
+long lastUpdate = 0;
 
 void setup() {
+  log_msg.data = logData;
+  log_msg.data_length = LOG_DATA_LEN;
+
   pinMode(pin, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
   nh.advertise(logger);
 
   lastTrigger = millis();
-  motorController.setTargetSpeed(2000);
+  startTime = lastTrigger + 5000;
 }
 
 
@@ -51,7 +62,8 @@ void loop() {
   motorController.update();
 
   long curTime = millis();
-  if (curTime - lastTrigger > triggerPeriod) {
+  if (lastUpdate < motorController.lastUpdate) {
+    lastUpdate = motorController.lastUpdate;
     lastTrigger = curTime;
     /*
     static int speed = 2000;
@@ -62,8 +74,15 @@ void loop() {
       motorController.setTargetSpeed(speed);
     }
     */
-    // logMsg(motorController.getSpeed());
-    logMsg(motor.getTicks());
+
+    long t = curTime - startTime;
+    // log(motor.getTicks(), t * 2, motorController.motorPower);
+    log(t * 2, motor.getTicks(), motorController.lastTicksDelta, motorController.lastInstantSpeed, motorController.motorPower);
+    // logMsg(motor.getTicks());
+  }
+
+  if (motorController.targetSpeed == 0 && curTime >= startTime) {
+    motorController.setTargetSpeed(2000);
   }
 }
 
